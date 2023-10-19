e8e06cd72306642a56dcf9940f718eac75f9b6f0
jannatul_axsa <jannatulaxajanifa586@gmail.com>
2023-10-19 21:58:27 +0600

    install all file and folder

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5005;
+
+// middleWare
+app.use(cors());
+app.use(express.json());
+
+// MongoDB COde:
+
+const uri = `mongodb+srv://${process.env.U_NAME}:${process.env.P_WORD}@cluster0.rsgizg7.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+   
+
+  
+   
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+   
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+  
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
