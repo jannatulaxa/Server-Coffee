f3974711be2041309fa49b1abb79d61bb0aa4d92
jannatul_axsa <jannatulaxajanifa586@gmail.com>
2023-10-20 18:44:48 +0600

    AddedProduct Section Added and read From database

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
-const port = process.env.PORT || 5005;
+const port = process.env.PORT || 5000;
 
 // middleWare
 app.use(cors());
@@ -11,7 +11,7 @@ app.use(express.json());
 
 // MongoDB COde:
 
-const uri = `mongodb+srv://${process.env.U_NAME}:${process.env.P_WORD}@cluster0.rsgizg7.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_U_NAME}:${process.env.DB_PASS}@cluster0.rsgizg7.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,6 +26,95 @@ async function run() {
   try {
    
 
+    // Database For All Product Adding By Admin
+    const AddProductCollection = client
+      .db("AddCoffeeDB")
+      .collection("Products");
+    // Database For Add Product in the cart
+    const AddCartProductCollection = client.db("CartCoffeeDB").collection("CartData");
+
+    // --------------------------------AddProductCollection Data Collection Server--------------------------------------
+
+    // General Products Section
+
+    // Insert General Data Into Database:
+    app.post("/addProduct", async (req, res) => {
+      const addingProducts = req.body;
+      const result = await AddProductCollection.insertOne(addingProducts);
+      res.send(result);
+    });
+    // Read All General Product From Database:
+    app.get("/addProduct", async (req, res) => {
+      const cursor = AddProductCollection.find();
+      const result = await cursor.toArray();
+
+      res.send(result);
+    });
+    // Read All General Product From Database:
+    app.get("/addProducts", async (req, res) => {
+      const cursor = AddProductCollection.find();
+      const result = await cursor.toArray();
+
+      res.send(result);
+    });
+    // Read All General Product From Database:
+    app.get("/addProducts/:brand_name", async (req, res) => {
+      const products = req.params;
+      const query = { brand_name: products.brand_name };
+      const cursor = AddProductCollection.find(query);
+      const result = await cursor.toArray();
+
+      res.send(result);
+    });
+    // POST and Read From Database:
+    app.post("/addProducts", async (req, res) => {
+      try {
+        const products = req.body; // This contains the brand_name sent from the frontend
+
+        // Use the brand_name to query your MongoDB collection
+        const query = { brand_name: products.brand_name };
+        const cursor = AddProductCollection.find(query);
+        const result = await cursor.toArray();
+        res.send(result);
+      
+      } catch (error) {
+        console.error("Error querying the database:", error);
+        res.status(500).send("Internal Server Error");
+      }
+    });
+
+    // Read With Id From All Cart Product From Database:
+    app.get("/addProduct/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await AddProductCollection.findOne(query);
+      res.send(result);
+    });
+
+    // Update Data in Database
+    app.patch("/addProduct/:id", async (req, res) => {
+      const id = req.params.id;
+      const data = req.body;
+      const query = { _id: new ObjectId(id) };
+
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          name: data.name,
+          brand_name:data.brand_name,
+          type:data.type,
+          price:data.price,
+          description:data.description,
+          rating:data.rating,
+          photo:data.photo,
+        },
+      };
+      const result = await AddProductCollection.updateOne(query, updateDoc, options);
+      res.send(result);
+    });
+
+
+
   
    
     console.log(
@@ -37,10 +126,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
-  
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+app.get("/", (req, res) => {
+  res.send("Hello I am REST API");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
